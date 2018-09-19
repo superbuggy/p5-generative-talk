8a9edcb5e3a0e5e8b1e221b1fad72a76a032aca1
James Reichard <jpqreichard@gmail.com>
2018-09-18 22:40:04 -0400

    Add particles, change color of lines

--- perlin/sketch.js
+++ perlin/sketch.js
@@ -3,6 +3,9 @@ let rows, columns
 let pixelsBetween = 10
 let timeOffset = 0
 
+let particles = []
+let flowField = []
+
 let frameRateDisplay = document.createElement('p')
 document.body.appendChild(frameRateDisplay)
 
@@ -11,6 +14,9 @@ function setup () {
   background(0, 0, 0)
   rows = floor(height / pixelsBetween)
   columns = floor(width / pixelsBetween)
+  for (let i = 0; i < 1000; i++) {
+    particles[i] = new Particle()
+  }
 }
 
 function draw () {
@@ -19,12 +25,14 @@ function draw () {
   for (let y = 0; y < rows; y++) {
     let xOffset = 0
     for (let x = 0; x < rows; x++) {
-      let index = (x + y * width) * 4
       let angle = noise(xOffset, yOffset, timeOffset) * TWO_PI
       let vector = p5.Vector.fromAngle(angle)
+      let flowFieldIndex = x + y * columns
+      flowField[flowFieldIndex] = vector
       xOffset += increment
 
-      stroke(0)
+      stroke(190)
+      strokeWeight(1)
       push()
       translate(x * pixelsBetween, y * pixelsBetween)
       rotate(vector.heading())
@@ -32,9 +40,55 @@ function draw () {
       pop()
     }
     yOffset += increment
-    timeOffset += 0.004
+    timeOffset += 0.001
+  }
+
+  for (let i = 0; i < particles.length; i++) {
+    // particles[i].follow(flowField)
+    particles[i].update()
+    particles[i].show()
   }
 
   frameRateDisplay.innerHTML = floor(frameRate())
 }
-console.log(2345)
+
+class Particle {
+  constructor () {
+    this.position = createVector(random(width), random(height))
+    // this.velocity = createVector(0, 0)
+    this.velocity = p5.Vector.random2D()
+    this.acceleration = createVector(0, 0)
+  }
+
+  update () {
+    this.velocity.add(this.acceleration)
+    this.position.add(this.velocity)
+    this.acceleration.mult(0)
+    this.edges()
+  }
+
+  applyForce (force) {
+    this.acceleration.add(force)
+  }
+
+  show () {
+    stroke(0)
+    strokeWeight(2)
+    point(this.position.x, this.position.y)
+  }
+
+  edges () {
+    if (this.position.x > width) this.position.x = 0
+    if (this.position.y > height) this.position.y = 0
+    if (this.position.x < 0) this.position.x = width
+    if (this.position.y < 0) this.position.y = height
+  }
+
+  follow (vectors) {
+    let x = floor(this.position.x / pixelsBetween)
+    let y = floor(this.position.y / pixelsBetween)
+    let index = x + y * cols
+    let force = vectors[index]
+    this.applyForce(force)
+  }
+}
