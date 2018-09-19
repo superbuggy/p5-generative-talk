4443870061ad044cfbc45eacf2fd72425e963fa8
James Reichard <jpqreichard@gmail.com>
2018-09-18 21:09:04 -0400

    convert to cool wavy lines

--- perlin/sketch.js
+++ perlin/sketch.js
@@ -1,7 +1,10 @@
 let increment = 0.1
 let rows, columns
 let pixelsBetween = 10
-let frameRateDisplay
+let timeOffset = 0
+
+let frameRateDisplay = document.createElement('p')
+document.body.appendChild(frameRateDisplay)
 
 function setup () {
   createCanvas(400, 400)
@@ -11,20 +14,26 @@ function setup () {
 }
 
 function draw () {
+  background(255)
   let yOffset = 0
   for (let y = 0; y < rows; y++) {
     let xOffset = 0
     for (let x = 0; x < rows; x++) {
       let index = (x + y * width) * 4
-      let randomness = noise(xOffset, yOffset) * 255
-      let vector = p5.Vector.fromAngle(0)
+      let angle = noise(xOffset, yOffset) * TWO_PI
+      let vector = p5.Vector.fromAngle(angle)
       xOffset += increment
-      noStroke()
-      // stroke(0)
-      fill(randomness)
-      rect(x * pixelsBetween, y * pixelsBetween, pixelsBetween, pixelsBetween)
+
+      stroke(0)
+      push()
+      translate(x * pixelsBetween, y * pixelsBetween)
+      rotate(vector.heading())
+      line(0, 0, pixelsBetween, 0)
+      pop()
     }
     yOffset += increment
   }
+
+  frameRateDisplay.innerHTML = floor(frameRate())
 }
 console.log(2345)
